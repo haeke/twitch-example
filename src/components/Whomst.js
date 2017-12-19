b5d1d5c458c88129463b2769b1479a794489fe89
Edwin Rivera <waehake@gmail.com>
2017-12-19 13:24:08 -0500

    updated the component that will show the list of users online or offline

--- src/components/Whomst.js
+++ src/components/Whomst.js
@@ -1,22 +1,54 @@
 import React, { Component } from 'react';
-import { Grid, Menu } from 'semantic-ui-react';
+import { Grid, Menu, Segment, Header } from 'semantic-ui-react';
 
 class Whomst extends Component {
   render() {
     return (
         <Grid columns='equal'>
           <Grid.Row>
-            <Grid.Column>
+            <Grid.Column> Left
             </Grid.Column>
             <Grid.Column width={8}>
-              <Menu fluid vertical>
-                <Menu.Item>First</Menu.Item>
-                <Menu.Item>Second</Menu.Item>
-                <Menu.Item>Third</Menu.Item>
-                <Menu.Item>Fourth</Menu.Item>
+              <Menu pointing secondary>
+                <Menu.Item name='first' />
+                <Menu.Item name='second' />
+                <Menu.Item name='third' />
+                <Menu.Item name='fourth' />
               </Menu>
+              <Segment>
+                <Grid columns='equal'>
+                  <Grid.Column width={3}>
+                    <img src='https://static-cdn.jtvnw.net/jtv_user_pictures/imaqtpie-profile_image-8efb10b7bed60d76-100x100.jpeg' alt='avatar'/>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Header textAlign='left'>First</Header>
+                    <Header.Subheader>Offline</Header.Subheader>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Header as='h2'>
+                      League of Legends
+                    </Header>
+                  </Grid.Column>
+                </Grid>
+              </Segment>
+              <Segment>
+                <Grid columns='equal'>
+                  <Grid.Column width={3}>
+                    <img src='https://static-cdn.jtvnw.net/jtv_user_pictures/imaqtpie-profile_image-8efb10b7bed60d76-100x100.jpeg' alt='avatar'/>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Header textAlign='left'>First</Header>
+                    <Header.Subheader>Offline</Header.Subheader>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Header as='h2'>
+                      League of Legends
+                    </Header>
+                  </Grid.Column>
+                </Grid>
+              </Segment>
             </Grid.Column>
-            <Grid.Column></Grid.Column>
+            <Grid.Column> Right</Grid.Column>
           </Grid.Row>
         </Grid>
     );
