2ecd17c25111d3f2356555b650bff21587a784af
Edwin Rivera <waehake@gmail.com>
2017-12-20 10:26:31 -0500

    added a function to query for the status of the userlist

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import CustomHeader from './CustomHeader';
 import Whomst from './Whomst';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+        users: ['cosmos', 'imaqtpie'],
+        results: [],
+      };
+  }
+
+  componentDidMount() {
+    var example = this.state.users.map((item) => axios.get(`https://api.twitch.tv/kraken/streams/${item}?client_id=geva5gbqt0vn2sh6q15z2xwh2vurlk`).then(res =>
+    console.log(res.data.stream)));
+    console.log(example);
+  }
+
   render() {
     return (
       <div className="container">
