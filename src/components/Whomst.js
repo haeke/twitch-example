3a4a98faf025a7fae7f3939d34f1d9e79c10f686
Edwin Rivera <waehake@gmail.com>
2017-12-20 13:03:14 -0500

    moved the method used to display information to the render method

--- src/components/Whomst.js
+++ src/components/Whomst.js
@@ -9,18 +9,34 @@ class Whomst extends Component {
   }
 
   whomstOnline() {
-    var other = this.props.results.map(item => {
-      if (item === null) {
-        console.log('nothing to see here');
-      } else {
-        console.log(item.channel.display_name);
-      }
-    });
-
-    return other;
+    console.log('test');
   }
 
   render() {
+    var info = this.props.results.map(item => {
+      if (item === null) {
+        console.log('nothing to see here');
+      } else {
+        return (
+        <Segment key={item.channel.display_name}>
+          <Grid columns='equal'>
+            <Grid.Column width={3}>
+              <img src='https://static-cdn.jtvnw.net/jtv_user_pictures/imaqtpie-profile_image-8efb10b7bed60d76-100x100.jpeg' alt='avatar'/>
+            </Grid.Column>
+            <Grid.Column>
+              <Header textAlign='left'>{item.channel.display_name}</Header>
+              <Header.Subheader>Online</Header.Subheader>
+            </Grid.Column>
+            <Grid.Column>
+              <Header as='h2'>
+                {item.game}
+              </Header>
+            </Grid.Column>
+          </Grid>
+        </Segment>
+      );
+      }
+    });
     return (
         <Grid columns='equal'>
           <Grid.Row>
@@ -33,6 +49,7 @@ class Whomst extends Component {
                 <Menu.Item name='third' />
                 <Menu.Item name='fourth' />
               </Menu>
+              {info}
               <Segment>
                 <Grid columns='equal'>
                   <Grid.Column width={3}>
