33fd7ea93dd52c94a7ad1374ae7799c15aa048bc
Edwin Rivera <waehake@gmail.com>
2017-12-22 10:59:16 -0500

    changed the styling again

--- src/components/Whomst.js
+++ src/components/Whomst.js
@@ -1,55 +1,53 @@
 import React, { Component } from 'react';
-import { Grid, Menu, Segment, Header } from 'semantic-ui-react';
+import { Grid, Header, Divider, Card } from 'semantic-ui-react';
 
 class Whomst extends Component {
   render() {
     var info = this.props.results.map((item, index) => {
       if (item === null) {
         return (
-        <Segment color="grey" key={this.props.users[index]}>
-          <Grid columns='equal'>
-            <Grid.Column width={3}>
-              <img
-                src='https://static-cdn.jtvnw.net/jtv_user_pictures/imaqtpie-profile_image-8efb10b7bed60d76-100x100.jpeg'
+            <Card key={this.props.users[index]}>
+              <img className="logo"
+                src='https://static-cdn.jtvnw.net/jtv_user_pictures/imaqtpie-profile_image-8efb10b7bed60d76-200x100.jpeg'
                 alt='avatar'/>
-            </Grid.Column>
-            <Grid.Column>
-              <Header textAlign='left'>{this.props.users[index]}</Header>
-              <Header.Subheader>Offline</Header.Subheader>
-            </Grid.Column>
-            <Grid.Column>
-              <Header as='h2'>
-                No Video
-              </Header>
-              <Header.Subheader>Going online at some point in time</Header.Subheader>
-            </Grid.Column>
-          </Grid>
-        </Segment>
+              <Card.Content>
+                <Card.Header>
+                  <Header textAlign='left'>{this.props.users[index]}</Header>
+                  <Header.Subheader>Offline</Header.Subheader>
+                </Card.Header>
+                <Card.Description>
+                  <Header as='h2'>
+                    No Active Stream
+                  </Header>
+                  <Header.Subheader>
+                    Will be online again soon
+                  </Header.Subheader>
+                </Card.Description>
+              </Card.Content>
+            </Card>
       );
       } else {
-        var logo =  'https://static-cdn.jtvnw.net/previews-ttv/live_user_' + item.channel.display_name.toLowerCase() + '-100x100.jpg';
+        var logo =  'https://static-cdn.jtvnw.net/previews-ttv/live_user_' + item.channel.display_name.toLowerCase() + '-200x100.jpg';
         return (
-        <Segment color='grey' key={item.channel.display_name}>
-          <Grid columns='equal'>
-            <Grid.Column width={3}>
-              <img src={logo} alt='avatar'/>
-            </Grid.Column>
-            <Grid.Column>
-                <a href={item.channel.url}>
-                  <Header textAlign='left'> {item.channel.display_name}</Header>
-                </a>
-              <Header.Subheader>Online</Header.Subheader>
-            </Grid.Column>
-            <Grid.Column>
-              <Header as='h2'>
-                {item.game}
-              </Header>
-              <Header.Subheader>
-                {item.channel.status}
-              </Header.Subheader>
-            </Grid.Column>
-          </Grid>
-        </Segment>
+            <Card key={item.channel.display_name}>
+              <img className="logo" src={logo} alt='avatar'/>
+              <Card.Content>
+                <Card.Header>
+                  <a href={item.channel.url}>
+                    <Header textAlign='left'> {item.channel.display_name}</Header>
+                  </a>
+                  <Header.Subheader>Online</Header.Subheader>
+                </Card.Header>
+                <Card.Description>
+                  <Header as='h2'>
+                    {item.game}
+                  </Header>
+                  <Header.Subheader>
+                    {item.channel.status}
+                  </Header.Subheader>
+                </Card.Description>
+              </Card.Content>
+            </Card>
       );
       }
     });
@@ -58,11 +56,11 @@ class Whomst extends Component {
           <Grid.Row>
             <Grid.Column>
             </Grid.Column>
-            <Grid.Column width={8}>
-              <Menu pointing secondary>
-                <Header as='h1'>User List</Header>
-              </Menu>
-              {info}
+            <Grid.Column width={9}>
+              <Divider />
+              <Grid columns={2} divided>
+                {info}
+              </Grid>
             </Grid.Column>
             <Grid.Column></Grid.Column>
           </Grid.Row>
